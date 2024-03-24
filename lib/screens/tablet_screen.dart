190149aa67b9b8b32ff458cf5dbdb2c347ee8a09
TaeBbong <mok05289@korea.ac.kr>
2024-03-24 23:07:48 +0900

    wip: ui

--- lib/screens/tablet_screen.dart
+++ lib/screens/tablet_screen.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
 
-class TabletScreen extends StatelessWidget {
+class TabletScreen extends StatefulWidget {
+  @override
+  _TabletScreenState createState() => _TabletScreenState();
+}
+
+class _TabletScreenState extends State<TabletScreen> {
+  Color background = Colors.black87;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tablet Layout'),
+        title: Text('TextColor Picker'),
       ),
       body: Row(
         children: [
-          NavigationRail(
-            selectedIndex: 0,
-            onDestinationSelected: (int index) {},
-            labelType: NavigationRailLabelType.selected,
-            destinations: const [
-              NavigationRailDestination(
-                icon: Icon(Icons.home),
-                label: Text('Home'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.search),
-                label: Text('Search'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.account_circle),
-                label: Text('Profile'),
-              ),
-            ],
+          Container(
+            child: Column(children: [
+              Text('Enter Background Color'),
+              ElevatedButton(onPressed: () {}, child: Text('Apply')),
+            ]),
           ),
           VerticalDivider(thickness: 1, width: 1),
           Expanded(
-            child: Center(
-              child: Text('This is the tablet layout'),
-            ),
+            child: Container(
+                color: background,
+                child: Column(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.4,
+                      child: Center(
+                        child: Text(
+                          'Lorem Ipsum',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.4,
+                      child: Center(
+                        child: Text(
+                          'Lorem Ipsum',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
           ),
         ],
       ),
